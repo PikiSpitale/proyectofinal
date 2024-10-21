9f7decb7f070bbfca8ec836d9a1da121e62aaccd
valentinoanashexgodinsta <vbruccilari@frsn.utn.edu.ar>
2024-10-21 18:46:10 -0300

    primer commint bruccinashe

--- ProyectoFinal/Venta.cs
+++ ProyectoFinal/Venta.cs
@@ -6,13 +6,26 @@ using System.Threading.Tasks;
 
 namespace ProyectoFinal
 {
-    internal class Venta
-    {        
+    internal class Venta : Vehiculo
+    {
         //Prop Privadas
-
+        private int id_cliente;
+        private DateTime fecha_compra;
+        private DateTime fecha_entrega;
+        private float subtotal;
+        private float iva;
+        private float descuento;
+        private float total;
 
         //Constructores
-
+        public Venta (int Id_cliente, DateTime Fecha_compra, DateTime Fecha_entrega, float Subtotal, float Iva, float Descuento, float Total)
+        {
+            this.id_cliente = Id_cliente;
+            this.subtotal = Subtotal;
+            this.iva = Iva;
+            this.descuento = Descuento;
+            this.total = Total;
+        }
 
 
         //Metodos
@@ -20,7 +33,40 @@ namespace ProyectoFinal
 
 
         //Prop publicas
-
+        public int Id_cliente
+        {
+            get { return this.id_cliente; }
+            set { this.id_cliente = value; }
+        }
+        public DateTime Fecha_compra
+        {
+            get => this.fecha_compra;
+        }
+        public DateTime Fecha_entrega
+        {
+            get => this.fecha_entrega;
+        }
+        public float Subtotal
+        {
+            get { return this.Subtotal; }
+            set { this.Subtotal = value; }
+        }
+        public float Iva
+        {
+            get { return this.iva; }
+            set { this.iva = value; }
+        }
+        public float Descuento
+        {
+            get { return this.descuento; }
+            set { this.descuento = value; }
+        }
+        public float Total
+        {
+            get { return this.total; }
+            set { this.total = value; }
+        }
+       
 
 
     }
